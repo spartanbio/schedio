36ec64cef3ef630ea4921b184e3aaf36d96151ad
alexkcollier <alexkcollier@gmail.com>
2018-09-07 09:10:30 -0400

    renames file

---
+++ src/components/docs/InteractionStates.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <BaseHeading level="1">
+      Interaction
+    </BaseHeading>
+    <p>
+      Interaction states visually communicate the status of components and interactive elements. These states should be visibly distinct, but the element should still be recognizable.
+    </p>
+
+    <BaseHeading level="2">
+      Motion &amp; Animation
+    </BaseHeading>
+    <p>
+      Motion and animation can be used to clarify and communicate that an element's state has changed. They also help make these transitions less jarring.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InteractionStates'
+}
+</script>
