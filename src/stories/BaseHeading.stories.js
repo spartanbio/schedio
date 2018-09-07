8e61c336e10e2f2cc69e5597ec9e0cface1513e1
alexkcollier <alexkcollier@gmail.com>
2018-09-07 09:15:19 -0400

    adds BaseHeading stories

---
+++ src/stories/BaseHeading.stories.js
@@ -0,0 +1,41 @@
+import { storiesOf } from '@storybook/vue'
+import { withKnobs, select, boolean } from '@storybook/addon-knobs'
+// import { action } from '@storybook/addon-actions'
+// import { linkTo } from '@storybook/addon-links'
+
+// Global registration doesn't work with `jsx` templates
+import BaseHeading from '@/components/base/BaseHeading.vue'
+
+storiesOf('BaseHeading', module)
+  .addDecorator(withKnobs)
+  .add('Headings', () => {
+    const headingLevel = select('Heading Level', [1, 2, 3], '1')
+
+    return {
+      render() {
+        return (
+          <BaseHeading level={headingLevel}>
+            Heading Level {headingLevel}
+          </BaseHeading>
+        )
+      }
+    }
+  })
+  .add('Display', () => {
+    const displayLevel = select('Display Level', [1, 2], '1')
+    const isUppercase = boolean('Uppercase', true)
+
+    return {
+      render() {
+        return (
+          <BaseHeading
+            level={displayLevel}
+            isDisplay
+            className={isUppercase && 'uppercase'}
+          >
+            Display Level {displayLevel}
+          </BaseHeading>
+        )
+      }
+    }
+  })
