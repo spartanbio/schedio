5cad2a32dcf42e6c8d97d0c5d0e9c5b0ae9b2e2c
alexkcollier <alexkcollier@gmail.com>
2018-09-07 09:09:41 -0400

    uses jsx templates for simplicity

--- src/stories/design.stories.js
+++ src/stories/design.stories.js
@@ -2,28 +2,13 @@ import { storiesOf } from '@storybook/vue'
 
 import Colour from '@/components/docs/Colour.vue'
 import Graphics from '@/components/docs/Graphics.vue'
-import Interaction from '@/components/docs/Interaction.vue'
+import InteractionStates from '@/components/docs/InteractionStates.vue'
 import Spacing from '@/components/docs/Spacing.vue'
 import Typography from '@/components/docs/Typography.vue'
 
 storiesOf('Design', module)
-  .add('Typography', () => ({
-    components: { Typography },
-    template: '<Typography />'
-  }))
-  .add('Colour', () => ({
-    components: { Colour },
-    template: '<Colour />'
-  }))
-  .add('Interaction (TODO)', () => ({
-    components: { Interaction },
-    template: '<Interaction />'
-  }))
-  .add('Graphics (TODO)', () => ({
-    components: { Graphics },
-    template: '<Graphics />'
-  }))
-  .add('Spacing', () => ({
-    components: { Spacing },
-    template: '<Spacing />'
-  }))
+  .add('Typography', () => h => <Typography />)
+  .add('Colour', () => h => <Colour />)
+  .add('Interaction (TODO)', () => h => <InteractionStates />)
+  .add('Graphics (TODO)', () => h => <Graphics />)
+  .add('Spacing', () => h => <Spacing />)
