fb60917a80d896da57bae8f70e9fac035e0cb793
alexkcollier <alexkcollier@gmail.com>
2018-09-13 09:46:12 -0400

    feat: revises interaction states

--- src/components/docs/InteractionStates.vue
+++ src/components/docs/InteractionStates.vue
@@ -4,14 +4,15 @@
       Interaction
     </BaseHeading>
     <p>
-      Interaction states visually communicate the status of components and interactive elements. These states should be visibly distinct, but the element should still be recognizable.
+      Interaction states communicate the status of components and interactive elements. These states should be visibly distinct, but the element should still be recognizable.
+      Interactive elements should whether or not they are interactive and provide feedback upon interaction. Feedback should be provided as to the outcome of that interaction.
     </p>
 
     <BaseHeading level="2">
       Motion &amp; Animation
     </BaseHeading>
     <p>
-      Motion and animation can be used to clarify and communicate that an element's state has changed. They also help make these transitions less jarring.
+      Motion and animation help smooth and clarify the transitions between states. They should never impede the usability or accessibility of a product when used correctly.
     </p>
   </div>
 </template>
