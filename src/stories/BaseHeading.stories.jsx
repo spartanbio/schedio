34b36804b2298b237bd65ef439e31b8588e12d92
alexkcollier <alexkcollier@gmail.com>
2018-09-14 09:55:49 -0400

    feat: Adds extra knobs

--- src/stories/BaseHeading.stories.jsx
+++ src/stories/BaseHeading.stories.jsx
@@ -1,37 +1,53 @@
-import { storiesOf } from '@storybook/vue'
-import { withKnobs, select, boolean } from '@storybook/addon-knobs'
-
 // Global registration doesn't work with `jsx` templates
+import { boolean, radios, select, text, withKnobs } from '@storybook/addon-knobs'
+import { storiesOf } from '@storybook/vue'
 import BaseHeading from '@/components/base/BaseHeading.vue'
 
 storiesOf('Components/BaseHeading', module)
   .addDecorator(withKnobs)
   .add('Headings', () => {
-    const headingLevel = select('Heading Level', [1, 2, 3], '1')
-    const isSubtle = boolean('Subtle', false)
+    const level = select('level', [1, 2, 3], '1', 'Required')
+    const slotContent = text('Slot content', '{{ Slot content }}', 'Required')
+    const isSubtle = boolean('is-subtle', false, 'Optional')
+
+    const props = {
+      level,
+      isSubtle
+    }
 
     return {
-      render() {
-        return (
-          <BaseHeading level={headingLevel} isSubtle={isSubtle}>
-            Heading Level {headingLevel}
-          </BaseHeading>
-        )
-      }
+      render: h => (
+        <BaseHeading {...{ props }}>
+          <p>Heading Level {level}</p>
+          <p>{slotContent}</p>
+        </BaseHeading>
+      )
     }
   })
   .add('Display', () => {
-    const displayLevel = select('Display Level', [1, 2], '1')
-    const isSubtle = boolean('Subtle', false)
-    const isUppercase = boolean('Uppercase', true)
+    const level = select('level', [1, 2], '1', 'Required')
+    const isDisplay = boolean('is-display', true, 'Required')
+    const slotContent = text('Slot content', '{{ Slot content }}', 'Required')
+    const options = radios(
+      'Pick one',
+      ['Default', 'is-uppercase', 'is-subtle'],
+      'Default',
+      'Optional'
+    )
+
+    const props = {
+      level,
+      isDisplay,
+      isUppercase: options === 'is-uppercase',
+      isSubtle: options === 'is-subtle'
+    }
 
     return {
-      render() {
-        return (
-          <BaseHeading level={displayLevel} isDisplay isSubtle={isSubtle} isUppercase={isUppercase}>
-            Display Level {displayLevel}
-          </BaseHeading>
-        )
-      }
+      render: h => (
+        <BaseHeading {...{ props }}>
+          <p>Display Level {level}</p>
+          <p>{slotContent}</p>
+        </BaseHeading>
+      )
     }
   })
