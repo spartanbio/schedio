be2651f955f85662a2036e035eb4b51dec03e1e3
alexkcollier <alexkcollier@gmail.com>
2018-09-17 12:52:27 -0400

    feat: adds button group

--- src/stories/BaseButton.stories.jsx
+++ src/stories/BaseButton.stories.jsx
@@ -2,19 +2,40 @@ import { storiesOf } from '@storybook/vue'
 import { select, text, withKnobs } from '@storybook/addon-knobs'
 import { colors, sizes } from '@/components/base/BaseButton/options'
 import BaseButton from '@/components/base/BaseButton'
+import ButtonGroup from '@/components/ButtonGroup'
+
+const colorOptions = ['', ...colors]
+const sizeOptions = ['', ...sizes]
 
 storiesOf('Components/BaseButton', module)
   .addDecorator(withKnobs)
   .add('Button', () => {
     const buttonText = text('Button text', 'Sample text')
-    const colorOptions = select('Button color', ['', ...colors])
-    const sizeOptions = select('Button size', ['', ...sizes], '')
+    const buttonColor = select('Button color', colorOptions)
+    const buttonSize = select('Button size', sizeOptions, '')
+
+    const props = {
+      buttonColor,
+      buttonSize
+    }
+
+    return {
+      render: h => <BaseButton {...{ props }}>{buttonText}</BaseButton>
+    }
+  })
+  .add('Button Group', () => {
+    const groupColor = select('Group color', colorOptions)
+    const button1Text = text('Button 1 text', 'Button 1')
+    const button2Text = text('Button 2 text', 'Button 2')
+    const button3Text = text('Button 3 text', 'Button 3')
 
     return {
       render: h => (
-        <BaseButton buttonColor={colorOptions} buttonSize={sizeOptions}>
-          {buttonText}
-        </BaseButton>
+        <ButtonGroup groupColor={groupColor}>
+          <BaseButton>{button1Text}</BaseButton>
+          <BaseButton>{button2Text}</BaseButton>
+          <BaseButton>{button3Text}</BaseButton>
+        </ButtonGroup>
       )
     }
   })
