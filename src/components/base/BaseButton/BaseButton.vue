be5ad6c6900483583a9a016eb0fb33876e0e3caa
alexkcollier <alexkcollier@gmail.com>
2018-09-17 10:12:19 -0400

    feat: adds button props

--- src/components/base/BaseButton/BaseButton.vue
+++ src/components/base/BaseButton/BaseButton.vue
@@ -1,14 +1,53 @@
 <template>
   <button
-    class="button button--color-night"
+    :class="classList"
+    class="button"
     @click="onClick">
     <slot/>
   </button>
 </template>
 
 <script>
+import { colors, sizes } from './options.js'
+
 export default {
   name: 'BaseButton',
+
+  props: {
+    buttonColor: {
+      type: String,
+      default: '',
+      validator: value => {
+        if (!value || colors.includes(value)) return true
+
+        return console.error(`\`buttonColor\` ${value} not found. Allowed colors: ${[...colors]}`)
+      }
+    },
+
+    buttonSize: {
+      type: String,
+      default: '',
+      validator: value => {
+        if (!value || sizes.includes(value)) return true
+
+        return console.error(`\`buttonSizes\` ${value} not found. Allowed sizes: ${sizes}`)
+      }
+    }
+  },
+
+  computed: {
+    classList() {
+      return [
+        // Handles storybook default
+        this.buttonColor ? `button--color-${this.buttonColor}` : 'button--color-green',
+        this.buttonSize ? `button--size-${this.buttonSize}` : ''
+      ]
+    }
+  },
+
+  mounted() {
+    console.log(this.$props)
+  },
 
   methods: {
     onClick() {
--- src/stories/BaseButton.stories.jsx
+++ src/stories/BaseButton.stories.jsx
@@ -1,12 +1,20 @@
 import { storiesOf } from '@storybook/vue'
-import { text, withKnobs } from '@storybook/addon-knobs'
+import { select, text, withKnobs } from '@storybook/addon-knobs'
+import { colors, sizes } from '@/components/base/BaseButton/options'
 import BaseButton from '@/components/base/BaseButton'
 
 storiesOf('Components/BaseButton', module)
   .addDecorator(withKnobs)
   .add('Button', () => {
-    const buttonText = text('Button text', 'I am a button')
+    const buttonText = text('Button text', 'Sample text')
+    const colorOptions = select('Button color', ['', ...colors])
+    const sizeOptions = select('Button size', ['', ...sizes], '')
+
     return {
-      render: h => <BaseButton>{buttonText}</BaseButton>
+      render: h => (
+        <BaseButton buttonColor={colorOptions} buttonSize={sizeOptions}>
+          {buttonText}
+        </BaseButton>
+      )
     }
   })
