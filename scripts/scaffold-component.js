9104f1ab233511129076b131fd34d8465e03d2dd
alexkcollier <alexkcollier@gmail.com>
2018-09-19 09:13:59 -0400

    feat: adds scaffold script

---
+++ scripts/scaffold-component.js
@@ -0,0 +1,65 @@
+const chalk = require('chalk')
+const fs = require('fs-extra')
+const path = require('path')
+
+// params
+const componentName = process.argv[2]
+const componentPath = /^Base[A-Z]\w+/.test(componentName) ? 'components/base' : 'components'
+const writeDir = path.resolve(__dirname, '../src', componentPath, componentName)
+
+// don't overwrite existing components
+if (fs.pathExistsSync(path.resolve(writeDir, `${componentName}.vue`))) {
+  return console.log(chalk.red(`\`${componentName}\` already exists!`))
+}
+
+// files and contents to write
+const scaffold = [
+  {
+    extension: 'vue',
+    contents: `<template>
+  <div>
+    <!-- component content -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: '${componentName}'
+}
+</script>\n`
+  },
+  {
+    extension: 'stories.jsx',
+    contents: `import { withKnobs } from '@storybook/addon-knobs'
+import { storiesOf } from '@storybook/vue'
+import ${componentName} from '@/${componentPath}/${componentName}'
+
+storiesOf('Components/${componentName}', module)
+  .addDecorator(withKnobs)
+  .add('${componentName}', () => {
+    return {
+      render: h => <${componentName} />
+    }
+  })\n`
+  },
+  {
+    extension: 'scss',
+    contents: '// component styles\n'
+  },
+  {
+    fileName: 'index',
+    extension: 'js',
+    contents: `export { default } from './${componentName}.vue'\n`
+  }
+]
+
+// set up promises
+const outputFiles = scaffold.map(({ fileName, extension, contents }) => {
+  const file = path.resolve(writeDir, `${fileName || componentName}.${extension}`)
+  fs.outputFile(file, contents)
+})
+
+// write files
+Promise.all(outputFiles)
+  .then(() => console.log(chalk.green(`Successfully scaffolded component \`${componentName}\``)))
+  .catch(err => console.error(chalk.red(err)))
