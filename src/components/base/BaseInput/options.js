d932c2dff8c95b5a9b4fd99e28506b18435ebf0c
alexkcollier <alexkcollier@gmail.com>
2018-09-19 11:40:51 -0400

    fix: adds additional input types

--- src/components/base/BaseInput/options.js
+++ src/components/base/BaseInput/options.js
@@ -1,1 +1,15 @@
-export const allowed = ['email', 'text', 'password', 'url', 'search', ''].sort()
+export const allowed = [
+  'text',
+  'email',
+  'number',
+  'password',
+  'search',
+  'tel',
+  'url',
+  'date',
+  'datetime-local',
+  'month',
+  'time',
+  'week',
+  ''
+].sort()
