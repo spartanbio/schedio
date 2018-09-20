6b296c7778dd3616b50157531c81488e63480ae0
alexkcollier <alexkcollier@gmail.com>
2018-09-20 11:33:33 -0400

    fix: registers base components

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import '@/assets/styles.scss'
+import registerComponentsGlobally from '@/utils/register-components-globally'
 
 Vue.config.productionTip = false
+
+// Automatically register base components
+const baseComponents = require.context(
+  '@/components/base', // path to components
+  true, // check subfolders?
+  /Base[A-Z]\w+\.(vue|js)$/ // regex file name
+)
+
+registerComponentsGlobally(baseComponents)
 
 new Vue({
   render: h => h(App)
