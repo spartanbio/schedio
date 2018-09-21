c8f419174b8715d2a4565b27dc708e62bfc397fc
alexkcollier <alexkcollier@gmail.com>
2018-09-21 14:04:23 -0400

    feat: adds row props

--- src/components/BaseContainer/BaseContainerRow.vue
+++ src/components/BaseContainer/BaseContainerRow.vue
@@ -1,11 +1,44 @@
 <template>
-  <div class="row">
+  <div :class="classList" class="row">
+    {{ columnJustification }}
     <slot/>
   </div>
 </template>
 
 <script>
+import { columnJustificationOptions } from './row-options'
+
 export default {
-  name: 'BaseContainerRow'
+  name: 'BaseContainerRow',
+
+  props: {
+    isVerticallyCentered: {
+      type: Boolean,
+      default: false
+    },
+
+    columnJustification: {
+      type: String,
+      default: '',
+      validator: value => {
+        if (!columnJustificationOptions.includes(value)) {
+          return console.error(
+            `\`columnJustification\` must be one of ${columnJustificationOptions}`
+          )
+        }
+
+        return true
+      }
+    }
+  },
+
+  computed: {
+    classList() {
+      return {
+        'row--vertically-centered': this.isVerticallyCentered,
+        [`row--columns-${this.columnJustification}`]: this.columnJustification
+      }
+    }
+  }
 }
 </script>
---
+++ src/components/BaseContainer/row-options.js
@@ -0,0 +1,1 @@
+export const columnJustificationOptions = ['', 'center', 'right']
