42117e9795ab6bdddfe88e4c1a9616248255d8ce
alexkcollier <alexkcollier@gmail.com>
2018-10-05 14:04:01 -0400

    chore: cleans up story

--- src/components/_docs/Typography/TypographyPrint.vue
+++ src/components/_docs/Typography/TypographyPrint.vue
@@ -3,54 +3,66 @@
     <BaseHeading level="1">Print Typography</BaseHeading>
     <p>
       <code>16pt</code> would be a bit large for most print typography, so instead, start with a
-      base of <code>10pt</code>. Increase or decrease this size as the print size requires. The
+      base of <code>{{ baseFontSize }}pt</code>. Increase or decrease this size as the print size requires using the
       calculator below can be used as a guide.
     </p>
 
-    <div>
-      <form >
-        <BaseInput
-          id="base-font-size"
-          v-model="baseFontSize"
-          :value="baseFontSize"
-          name="base-font-size"
-          label="Base font size (pt)"/>
-      </form>
+    <blockquote>
+      <strong>Note:</strong> The calculator below is <em>only</em> for font size. Other than size,
+      all other parameters should match those in
+      <a href="#" @click.prevent="linkToComponent">Headings and Displays</a>.
+    </blockquote>
 
-      <table class="table table--bordered table--hoverable">
-        <thead>
-          <tr>
-            <th class="table__cell"/>
-            <th class="table__cell">Size</th>
-            <th class="table__cell">Leading</th>
-            <th class="table__cell">Tracking</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="{ name, value } in fontSizes" :key="name">
-            <td class="table__cell" style="text-transform: capitalize;">
-              {{ name | prettyName }}
-            </td>
-            <td class="table__cell table__cell--numeric">
-              <code>{{ computePrintSize(value) }}</code>
-            </td>
-            <td class="table__cell table__cell--numeric">
-              <code>{{ computeLeading(value) }}</code>
-            </td>
-            <td class="table__cell table__cell--numeric">
-              <code>{{ computeTracking(value) }}</code>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <BaseHeading level="2">Typography Calculator</BaseHeading>
+
+    <form >
+      <BaseInput
+        id="base-font-size"
+        v-model="baseFontSize"
+        :value="baseFontSize"
+        name="base-font-size"
+        label="Base font size (pt)"/>
+    </form>
+
+    <table class="table table--bordered table--hoverable">
+      <thead>
+        <tr>
+          <th class="table__cell"/>
+          <th class="table__cell">Size</th>
+          <th class="table__cell">Leading</th>
+          <th class="table__cell">Tracking</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="{ name, value } in fontSizes" :key="name">
+          <td class="table__cell" style="text-transform: capitalize;">
+            {{ name | prettyName }}
+          </td>
+          <td class="table__cell table__cell--numeric">
+            <code>{{ computePrintSize(value) }}</code>
+          </td>
+          <td class="table__cell table__cell--numeric">
+            <code>{{ computeLeading(value) }}</code>
+          </td>
+          <td class="table__cell table__cell--numeric">
+            <code>{{ computeTracking(value) }}</code>
+          </td>
+          <td class="table__cell">
+            <span :style="{ fontSize: computePrintSize(value) }">
+              Example text
+            </span>
+          </td>
+        </tr>
+      </tbody>
+    </table>
 
   </div>
 </template>
 
 <script>
-import { props } from '@/assets/styles/tokens/dist/tokens.raw.json'
+import { linkTo } from '@storybook/addon-links'
 import orderBy from 'lodash.orderby'
+import { props } from '@/assets/styles/tokens/dist/tokens.raw.json'
 
 const fontSizes = Object.values(props).filter(
   ({ category, name }) => category === 'font-size' && name !== 'base-font-size'
@@ -64,7 +76,7 @@ export default {
   },
 
   data: () => ({
-    baseFontSize: 10,
+    baseFontSize: 11,
     fontSizes: orderBy(fontSizes, 'name')
   }),
 
@@ -79,7 +91,9 @@ export default {
     },
     computeTracking(value) {
       return value.replace('rem', '') > 3 ? 0.025 : 0
-    }
+    },
+
+    linkToComponent: linkTo('Design.Typography', 'Headings & Displays')
   }
 }
 </script>
