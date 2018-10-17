306224c3f11e40e49e7f817d9289f9ab37380f9a
alexkcollier <alexkcollier@gmail.com>
2018-10-17 13:57:51 -0400

    docs(base-input): adds some dcumentation

--- src/components/BaseInput/BaseInput.stories.jsx
+++ src/components/BaseInput/BaseInput.stories.jsx
@@ -1,24 +1,37 @@
+import BaseHeading from '@/components/BaseHeading'
+import BaseInput from '@/components/BaseInput'
+import { allowed as options } from '@/components/BaseInput/options'
+import PropList from '@/components/_docs/PropList'
+import StoryContainer from '@/components/_docs/StoryContainer'
 import { boolean, select, text, withKnobs } from '@storybook/addon-knobs'
 import { storiesOf } from '@storybook/vue'
-import { allowed as options } from '@/components//BaseInput/options'
-import BaseInput from '@/components//BaseInput'
 
 storiesOf('Base Components/BaseInput', module)
   .addDecorator(withKnobs)
   .add('Input', () => {
     const props = {
-      id: text('ID', 'base-input', 'Required'),
-      name: text('name', 'base-input', 'Required'),
-      label: text('Label', 'Label text', 'Required'),
-      placeholder: text('Placeholder', 'Placeholder text', 'Required'),
-      type: select('Input type', options, '', 'Optional'),
-      isInline: boolean('isInline', false, 'Optional'),
-      isFullwidth: boolean('isFullwidth', false, 'Optional')
+      id: text('ID', 'base-input', 'Required Props'),
+      name: text('name', 'base-input', 'Required Props'),
+      label: text('Label', 'Label text', 'Required Props'),
+      placeholder: text('Placeholder', 'Placeholder text', 'Required Props'),
+      type: select('Input type', options, '', 'Optional Props'),
+      isInline: boolean('isInline', false, 'Optional Props'),
+      isFullwidth: boolean('isFullwidth', false, 'Optional Props')
     }
 
-    const isDisabled = boolean('isDisabled', false, 'Optional')
+    const isDisabled = boolean('isDisabled', false, '$attrs')
 
     return {
-      render: h => <BaseInput {...{ props }} disabled={isDisabled} />
+      render: h => (
+        <StoryContainer>
+          <BaseHeading level="1">Input</BaseHeading>
+          <p>Inputs are used to collect user provided text data that should be fairly short.</p>
+
+          <BaseHeading level="2">Example</BaseHeading>
+          <BaseInput {...{ props }} disabled={isDisabled} />
+
+          <PropList component={BaseInput} />
+        </StoryContainer>
+      )
     }
   })
