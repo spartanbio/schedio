6e748e7409fd7eade1281201896ee378b741941d
alexkcollier <alexkcollier@gmail.com>
2018-10-17 14:32:45 -0400

    docs(base-radio): adds some documentation

--- src/components/BaseRadio/BaseRadio.stories.jsx
+++ src/components/BaseRadio/BaseRadio.stories.jsx
@@ -1,33 +1,38 @@
-import { storiesOf } from '@storybook/vue'
+import BaseHeading from '@/components/BaseHeading'
+import BaseRadio from '@/components/BaseRadio'
+import PropList from '@/components/_docs/PropList'
+import StoryContainer from '@/components/_docs/StoryContainer'
+import StoryLink from '@/components/_docs/StoryLink'
 import { boolean, text, withKnobs } from '@storybook/addon-knobs'
-import BaseRadio from '@/components//BaseRadio'
+import { storiesOf } from '@storybook/vue'
 
 storiesOf('Base Components/BaseRadio', module)
   .addDecorator(withKnobs)
   .add('Radio', () => {
-    const name = text('Name', 'radio-group', 'Required')
+    const name = text('Name', 'radio-group', 'Required props')
+
     const radios = [
       {
-        id: text('ID', 'radio-1', 'Required'),
-        label: text('Label', 'Radio 1', 'Required'),
-        isReversed: boolean('isReversed', false, 'Optional'),
-        isDisabled: boolean('Radio 1 Disabled', false, 'Optional'),
+        id: text('Radio 1 ID', 'radio-1', 'Required props'),
+        label: text('Radio 1 Label', 'Radio 1', 'Required props'),
+        isReversed: boolean('isReversed', false, 'Optional props'),
+        isDisabled: boolean('Radio 1 Disabled', false, '$attrs'),
         name,
         value: 'radio 1'
       },
       {
-        id: text('ID', 'radio-2', 'Required'),
-        label: text('Label', 'Radio 2', 'Required'),
-        isReversed: boolean('isReversed', false, 'Optional'),
-        isDisabled: boolean('Radio 2 Disabled', false, 'Optional'),
+        id: text('Radio 2 ID', 'radio-2', 'Required props'),
+        label: text('Radio 2 Label', 'Radio 2', 'Required props'),
+        isReversed: boolean('isReversed', false, 'Optional props'),
+        isDisabled: boolean('Radio 2 Disabled', false, '$attrs'),
         name,
         value: 'radio 2'
       },
       {
-        id: text('ID', 'radio-3', 'Required'),
-        label: text('Label', 'Radio 3', 'Required'),
-        isReversed: boolean('isReversed', false, 'Optional'),
-        isDisabled: boolean('Radio 3 Disabled', false, 'Optional'),
+        id: text('Radio 3 ID', 'radio-3', 'Required props'),
+        label: text('Radio 3 Label', 'Radio 3', 'Required props'),
+        isReversed: boolean('isReversed', false, 'Optional props'),
+        isDisabled: boolean('Radio 3 Disabled', false, '$attrs'),
         name,
         value: 'radio 3'
       }
@@ -35,11 +40,20 @@ storiesOf('Base Components/BaseRadio', module)
 
     return {
       render: h => (
-        <div>
+        <StoryContainer>
+          <BaseHeading level="1">Radios</BaseHeading>
+          <p>
+            Radios provide the ability to make a single choice from a list of options. If the list
+            is very long, consider using a <StoryLink to="Base Components/BaseSelect" />.
+          </p>
+
+          <BaseHeading level="2">Example</BaseHeading>
           {radios.map(radio => (
             <BaseRadio {...{ props: radio }} disabled={radio.isDisabled} value={radio.value} />
           ))}
-        </div>
+
+          <PropList component={BaseRadio} />
+        </StoryContainer>
       )
     }
   })
