6747ecabbc75f9756f1f4977233f8bd46c2767b5
alexkcollier <alexkcollier@gmail.com>
2018-10-17 15:01:50 -0400

    docs(button-group): adds some documentation

--- src/components/ButtonGroup/ButtonGroup.stories.jsx
+++ src/components/ButtonGroup/ButtonGroup.stories.jsx
@@ -1,26 +1,37 @@
-import { storiesOf } from '@storybook/vue'
-import { select, text, withKnobs } from '@storybook/addon-knobs'
-import { colors } from '@/components//BaseButton/options'
-import BaseButton from '@/components//BaseButton'
+import BaseButton from '@/components/BaseButton'
+import { colors } from '@/components/BaseButton/options'
 import ButtonGroup from '@/components/ButtonGroup'
+import BaseHeading from '@/components/BaseHeading'
+import PropList from '@/components/_docs/PropList'
+import StoryContainer from '@/components/_docs/StoryContainer'
+import { select, text, withKnobs } from '@storybook/addon-knobs'
+import { storiesOf } from '@storybook/vue'
 
 const colorOptions = ['', ...colors]
 
 storiesOf('Components/ButtonGroup', module)
   .addDecorator(withKnobs)
   .add('Button Group', () => {
-    const groupColor = select('Group color', colorOptions, '', 'Optional')
-    const button1Text = text('Button 1 text', 'Button 1', 'Required')
-    const button2Text = text('Button 2 text', 'Button 2', 'Required')
-    const button3Text = text('Button 3 text', 'Button 3', 'Required')
+    const groupColor = select('Group color', colorOptions, '', 'Optional props')
+    const button1Text = text('Button 1 text', 'Button 1', 'Slots')
+    const button2Text = text('Button 2 text', 'Button 2', 'Slots')
+    const button3Text = text('Button 3 text', 'Button 3', 'Slots')
 
     return {
       render: h => (
-        <ButtonGroup groupColor={groupColor}>
-          <BaseButton>{button1Text}</BaseButton>
-          <BaseButton>{button2Text}</BaseButton>
-          <BaseButton>{button3Text}</BaseButton>
-        </ButtonGroup>
+        <StoryContainer>
+          <BaseHeading level="1">Button Group</BaseHeading>
+          <p>Button groups take individual buttons, and merge them into a single group.</p>
+
+          <BaseHeading level="2">Example</BaseHeading>
+          <ButtonGroup groupColor={groupColor}>
+            <BaseButton>{button1Text}</BaseButton>
+            <BaseButton>{button2Text}</BaseButton>
+            <BaseButton>{button3Text}</BaseButton>
+          </ButtonGroup>
+
+          <PropList component={ButtonGroup} />
+        </StoryContainer>
       )
     }
   })
