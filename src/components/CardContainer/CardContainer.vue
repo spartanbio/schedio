9bd3d93e4297d09bbb6ac75cd6470829598992b2
alexkcollier <alexkcollier@gmail.com>
2018-10-23 12:04:16 -0400

    feat: adds card container

---
+++ src/components/CardContainer/CardContainer.vue
@@ -0,0 +1,19 @@
+<template>
+  <BaseContainer>
+    <CardDisplay>
+      <slot />
+    </CardDisplay>
+  </BaseContainer>
+</template>
+
+<script>
+import CardDisplay from '@/components/CardDisplay'
+
+export default {
+  name: 'CardContainer',
+
+  components: {
+    CardDisplay
+  }
+}
+</script>
