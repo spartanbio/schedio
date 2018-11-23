7bc05cc46c83619ff7c32debfb2906c7c16b4145
alexkcollier <alexkcollier@gmail.com>
2018-11-23 13:23:13 -0500

    fix(inputs): imports label

--- src/mixins/InputControl.mixin.js
+++ src/mixins/InputControl.mixin.js
@@ -1,8 +1,13 @@
 // Checkboxes, radios, etc.
+import { SLabel } from '@/components/SLabel'
 import InputListeners from './InputListeners.mixin'
 import InputProps from './InputProps.mixin'
 
 export default {
+  components: {
+    SLabel
+  },
+
   // prevent `<label/>` from inheriting attrs
   inheritAttrs: false,
 
