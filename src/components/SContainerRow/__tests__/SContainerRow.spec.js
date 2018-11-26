4fd33b478de470ba722ed72d08ad751fe2e7b4db
alexkcollier <alexkcollier@gmail.com>
2018-11-26 11:00:25 -0500

    test(container-row): adds container-row tests

---
+++ src/components/SContainerRow/__tests__/SContainerRow.spec.js
@@ -0,0 +1,50 @@
+import mobileBreakpoints from '@/components/SContainer/mobile-breakpoints'
+import SContainerRow from '@/components/SContainerRow/SContainerRow.vue'
+import rowOptions from '@/components/SContainerRow/row-options'
+import { shallowMount } from '@vue/test-utils'
+import camelCase from 'lodash.camelcase'
+
+describe('SContainerRow.vue', () => {
+  const staticProps = ['justify', 'align']
+  // Generate the same props generated in `SContainerRow.vue`
+  const propsToGenerate = ['align-until', 'align-after', 'justify-until', 'justify-after']
+  const propList = propsToGenerate.reduce((list, prop) => {
+    mobileBreakpoints.forEach(breakPoint => list.push(camelCase(`${prop}-${breakPoint}`)))
+    return list
+  }, staticProps)
+
+  const rowPropTest = (propName, value) => {
+    wrapper.setProps({ [propName]: value })
+    expect(wrapper.props(propName)).toBeTruthy()
+    expect(wrapper.html()).toMatchSnapshot()
+  }
+
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = shallowMount(SContainerRow, {
+      slots: {
+        default: '<div>Row Content</div>'
+      }
+    })
+  })
+
+  it('renders correctly', () => {
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('can be reversed', () => {
+    rowPropTest('isReversed', true)
+  })
+
+  it('generates props for mobile breakpoints', () => {
+    propList.forEach(expect(wrapper.props()).toHaveProperty)
+  })
+
+  propList.forEach(propName => {
+    const flexOptions = Object.values(rowOptions).filter(opt => !!opt)
+    flexOptions.forEach(option => {
+      it(`can be ${propName} ${option}`, () => rowPropTest(propName, option))
+    })
+  })
+})
