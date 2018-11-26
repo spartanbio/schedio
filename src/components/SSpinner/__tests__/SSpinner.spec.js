3fe04d4e6768a86ccb01bc3f6130de016fe09848
alexkcollier <alexkcollier@gmail.com>
2018-11-26 14:48:41 -0500

    test(spinner): adds spinner test

---
+++ src/components/SSpinner/__tests__/SSpinner.spec.js
@@ -0,0 +1,60 @@
+import { shallowMount } from '@vue/test-utils'
+import * as options from '@/components/SSpinner/options'
+import SSpinner from '@/components/SSpinner/SSpinner.vue'
+
+describe('SSpinner.vue', () => {
+  let wrapper
+  const errorSpy = jest.spyOn(global.console, 'error').mockImplementation(() => {})
+
+  beforeEach(() => {
+    wrapper = shallowMount(SSpinner, {
+      propsData: {
+        progress: '50'
+      }
+    })
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('renders correctly', () => {
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  options.colors.forEach(color => {
+    it(`can be ${color}`, () => {
+      wrapper.setProps({ color })
+      expect(errorSpy).not.toBeCalled()
+      expect(wrapper.props('color')).toBe(color)
+      expect(wrapper.html()).toMatchSnapshot()
+    })
+  })
+
+  it('only allows valid colors', () => {
+    shallowMount(SSpinner, {
+      propsData: {
+        color: 'this is not a color'
+      }
+    })
+    expect(errorSpy).toBeCalled()
+  })
+
+  options.sizes.forEach(size => {
+    it(`can be ${size}`, () => {
+      wrapper.setProps({ size })
+      expect(errorSpy).not.toBeCalled()
+      expect(wrapper.props('size')).toBe(size)
+      expect(wrapper.html()).toMatchSnapshot()
+    })
+  })
+
+  it('only allows valid sizes', () => {
+    shallowMount(SSpinner, {
+      propsData: {
+        size: 'this is not a size'
+      }
+    })
+    expect(errorSpy).toBeCalled()
+  })
+})
