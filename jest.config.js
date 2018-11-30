b49b8e9c032ebc865ac96075017f96698e0dc6e9
alexkcollier <alexkcollier@gmail.com>
2018-11-30 09:00:48 -0500

    test: mocks static files

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,9 @@ module.exports = {
     '^.+\\.jsx?$': '<rootDir>/node_modules/babel-jest'
   },
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1'
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/file-mock.js'
   },
   snapshotSerializers: ['jest-serializer-vue'],
   testMatch: ['**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'],
