a994e9b18fcc8c208efbbe692fdc63058487ee1c
alexkcollier <alexkcollier@gmail.com>
2019-04-03 09:14:48 -0400

    docs: adds Adobe XD components download

--- src/docs/design.stories.jsx
+++ src/docs/design.stories.jsx
@@ -6,6 +6,8 @@ import TypographyBase from '@/docs/Typography/TypographyBase.vue'
 import TypographyHeadings from '@/docs/Typography/TypographyHeadings.vue'
 import TypographyPrint from '@/docs/Typography/TypographyPrint.vue'
 import { storiesOf } from '@storybook/vue'
+import { SHeading } from '@/components/SHeading/index'
+import { SLink } from '@/components/SLink/index'
 
 const hideAddons = {
   options: {
@@ -13,7 +15,27 @@ const hideAddons = {
   }
 }
 
+storiesOf('* Design/Assets', module).add('Design Assets', () => ({
+  render(h) {
+    return (
+      <StoryContainer>
+        <SHeading>Design assets</SHeading>
+        <p>
+          To use these components and styles in Adobe XD,{' '}
+          <SLink to="assets/schedio-components-01.xd">use this file</SLink>.
+        </p>
+
+        <p>
+          The latest version of that file can be found on Spartan's server at{' '}
+          <code>S:\Sales & Marketing\Marketing and UI\Schedio Components_01.xd</code>
+        </p>
+      </StoryContainer>
+    )
+  }
+}))
+
 storiesOf('* Design/Color', module).add('Color', () => ({ render: h => <Color /> }), hideAddons)
+
 storiesOf('* Design/Interaction', module).add(
   'Interaction',
   () => ({ render: h => <Interaction /> }),
