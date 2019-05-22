c0eeb5b7b118e3c8aa571f0d96e94dfca4ca32bf
alexkcollier <alexkcollier@gmail.com>
2019-05-22 15:39:12 -0400

    test: adds config for vscode-jest

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,8 @@
-module.exports = {
-  presets: ['@vue/app', '@babel/preset-env'],
-  plugins: ['transform-vue-jsx', '@babel/plugin-transform-runtime']
+module.exports = api => {
+  api.cache(true)
+
+  return {
+    presets: ['@vue/app', '@babel/preset-env'],
+    plugins: ['transform-vue-jsx', '@babel/plugin-transform-runtime']
+  }
 }
