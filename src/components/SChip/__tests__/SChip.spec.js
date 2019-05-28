852008a04d4e242a2ff2f288eb44e025109e3895
alexkcollier <alexkcollier@gmail.com>
2019-05-28 12:21:09 -0400

    feat(chip): adds chip component

---
+++ src/components/SChip/__tests__/SChip.spec.js
@@ -0,0 +1,114 @@
+import { shallowMount, mount } from '@vue/test-utils'
+import SChip from '@/components/SChip/SChip.vue'
+import SButton from '@/components/SButton/SButton.vue'
+import { colors } from '../options'
+
+describe('SChip.vue', () => {
+  const errorSpy = jest.spyOn(global.console, 'error').mockImplementation(() => {})
+  let wrapper
+
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+
+  describe('base', () => {
+    beforeEach(() => {
+      wrapper = shallowMount(SChip, {
+        slots: {
+          default: 'Slot content'
+        }
+      })
+    })
+
+    it('renders correctly', () => {
+      expect(wrapper.html()).toMatchSnapshot()
+    })
+
+    it.each(colors)('can have color %s', color => {
+      const colorWrapper = shallowMount(SChip, { propsData: { color } })
+
+      expect(errorSpy).not.toBeCalled()
+      expect(colorWrapper.html()).toMatchSnapshot()
+    })
+
+    it('validates colors', () => {
+      shallowMount(SChip, { propsData: { color: 'Not a color' } })
+      expect(errorSpy).toBeCalledTimes(2)
+    })
+
+    it('can have its HTML tag set', () => {
+      wrapper.setProps({ tag: 'li' })
+      expect(wrapper.contains('li')).toBe(true)
+      expect(wrapper.html()).toMatchSnapshot()
+    })
+  })
+
+  describe('closable', () => {
+    beforeEach(() => {
+      wrapper = mount(SChip, {
+        slots: {
+          default: 'Slot content'
+        },
+        propsData: {
+          isClosable: true
+        }
+      })
+    })
+
+    it('can be closable', () => {
+      expect(wrapper.contains(SButton)).toBe(true)
+      expect(wrapper.html()).toMatchSnapshot()
+    })
+
+    it('emits a close event when the button is clicked', () => {
+      const button = wrapper.find(SButton)
+      button.trigger('click')
+      expect(wrapper.emitted('close')).toBeTruthy()
+    })
+
+    it('is hidden on close', () => {
+      const chips = {
+        chip1: true,
+        chip2: true,
+        chip3: true
+      }
+      const closeWrapper = mount({
+        name: 'CloseWrapper',
+        components: { SChip },
+        data() {
+          return {
+            chips
+          }
+        },
+        template: `
+          <div>
+            <SChip
+              v-for="(val, chip) in chips"
+              :key="chip"
+              is-closable="true"
+              v-model="chips[chip]"
+            >
+              {{ chip }}
+            </SChip>
+          </div>
+        `
+      })
+
+      const chipComponents = closeWrapper.findAll(SChip)
+      const buttons = closeWrapper.findAll(SButton)
+      const chipNames = Object.keys(chips)
+      const totalChips = chipNames.length
+
+      expect(chipComponents.length).toBe(totalChips)
+      expect(buttons.length).toBe(totalChips)
+
+      const randIdx = Math.floor(totalChips * Math.random())
+      const randKey = chipNames[randIdx]
+
+      buttons.at(randIdx).trigger('click')
+
+      expect(chipComponents.at(randIdx).isVisible()).toBe(false)
+      expect(closeWrapper.vm.chips[randKey]).toBe(false)
+    })
+  })
+})
