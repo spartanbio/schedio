8ba785a6ea26fe7a1cbf0776bb8fb53abd088c29
alexkcollier <alexkcollier@gmail.com>
2019-05-30 14:57:47 -0400

    fix(file-input): emits files on input

--- src/components/SFileInput/SFileInput.vue
+++ src/components/SFileInput/SFileInput.vue
@@ -184,6 +184,7 @@ export default {
       }
 
       this.fileList = this.$attrs.multiple ? [...this.fileList, ...valid] : valid
+      this.$emit('input', { target: { files: this.fileList } })
     },
     /**
      * Validates files
