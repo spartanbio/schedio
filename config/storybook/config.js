19a1d67b02553da3a9b099191bba066afc3d1b14
alexkcollier <alexkcollier@gmail.com>
2019-06-05 09:31:41 -0400

    feat(accessibility): adds `@storybook/addon-a11y`

--- config/storybook/config.js
+++ config/storybook/config.js
@@ -1,9 +1,11 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import Schedio from '@'
+import { addDecorator, addParameters, configure } from '@storybook/vue'
 import { withTests } from '@storybook/addon-jest'
 import { withKnobs } from '@storybook/addon-knobs'
+// a11y must be imported after storybook prior to @storybook/vue@5.1.1
+import { withA11y } from '@storybook/addon-a11y'
 import { create } from '@storybook/theming'
-import { addDecorator, addParameters, configure } from '@storybook/vue'
 import Vue from 'vue'
 import results from '../../.jest-test-results.json'
 import StoryContainer from '@@/docs/components/StoryContainer.vue'
@@ -34,6 +36,7 @@ addParameters({
 })
 
 addDecorator(withKnobs)
+addDecorator(withA11y)
 addDecorator(withTests({ results }))
 addDecorator(story => ({
   name: 'TheStory',
