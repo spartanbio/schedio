659af7f13fa2f6ab7ff95392f03ca700ae4c39c3
alexkcollier <alexkcollier@gmail.com>
2019-07-24 12:15:48 -0400

    style: lint

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,8 @@
-module.exports = api => {
+module.exports = (api) => {
   api.cache(true)
 
   return {
     presets: ['@vue/app', '@babel/preset-env'],
-    plugins: ['transform-vue-jsx', '@babel/plugin-transform-runtime']
+    plugins: ['transform-vue-jsx', '@babel/plugin-transform-runtime'],
   }
 }
