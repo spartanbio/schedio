55086084340161cd4c23f824ec1d48b775f72d09
alexkcollier <alexkcollier@gmail.com>
2019-12-10 15:43:44 -0500

    fix: uses correct `style-resources-loader` config

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,20 @@
-const sharedConf = require('./config/shared-webpack-conf')
+const { aliases, styleResourcesLoaderOptions } = require('./config/shared-webpack-conf')
 
 module.exports = {
   productionSourceMap: false,
   lintOnSave: true,
   pluginOptions: {
-    'style-resources-loader': sharedConf.styleResourcesLoaderOptions,
+    'style-resources-loader': {
+      preProcessor: 'scss',
+      patterns: styleResourcesLoaderOptions.patterns,
+    },
   },
   css: {
     extract: true,
   },
   configureWebpack: {
     resolve: {
-      alias: sharedConf.aliases,
+      alias: aliases,
     },
   },
 }
