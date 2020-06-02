a0e33c480f6e20c31e3064edf25a01feb3d5206e
Alex Collier <alexkcollier@gmail.com>
2020-06-02 10:48:27 -0400

    chore: replace `@semantic-release/gitlab` w/`@semantic-release/github`

--- release.config.js
+++ release.config.js
@@ -5,7 +5,7 @@ module.exports = {
     '@semantic-release/changelog',
     '@semantic-release/npm',
     [
-      '@semantic-release/gitlab',
+      '@semantic-release/github',
       {
         assets: [
           { path: 'dist/**/*', label: 'schedio' },
